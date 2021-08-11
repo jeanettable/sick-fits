5e5ed08df87596bbd8549ed84759d62ca7ba0cf4
Jeanettable <jeanetteabell@gmail.com>
2021-08-11 16:20:18 -0600

    Nextjs pages directory set up endpoints with a base Page component wrapper

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+export default function Page(props) {
+  const { children, cool } = props;
+  return (
+    <div>
+      <h4>{cool}</h4>
+      <h2>I am the Page component.</h2>
+      {children}
+    </div>
+  );
+}
+
+Page.propTypes = {
+  cool: PropTypes.string,
+  children: PropTypes.oneOf([       // or PropTypes.any
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
---
+++ sick-fits/frontend/pages/_app.js
@@ -0,0 +1,10 @@
+// for controlling higher up in your pages, must go into _app.js
+import Page from '../components/Page';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
+}
