c18d2d73888ed391013670bcff6b7eec74bfc37e
Jeanettable <jeanetteabell@gmail.com>
2021-08-12 15:21:00 -0600

    FE : set up getInitialProps to collect style sheets

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,6 +1,19 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  // getInitialProps is an async function that allows initial data population
+  // used for **SEO** (search engine optimization)
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    // console.log('styleTags>>>', styleTags);
+    return {...page, styleTags};
+  }
+
   render() {
     return (
       <Html lang="en-CA">
