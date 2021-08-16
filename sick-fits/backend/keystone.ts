f14d3a69ec7326e06e7a3a51c200d754f8178235
Jeanettable <jeanetteabell@gmail.com>
2021-08-16 17:41:53 -0600

    BE : seeding connectivity through db object done

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -5,6 +5,7 @@ import { withItemData, statelessSessions } from '@keystone-next/keystone/session
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
     process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
@@ -36,7 +37,13 @@ export default withAuth(config({
     db: {
         adapter: 'mongoose',
         url: databaseURL,
-        // todo: add data seeding here
+        // seeding connectivity here:
+        async onConnect(keystone) {
+            console.log('Connected to the database.');
+            if(process.argv.includes('--seed-data')) {
+                await insertSeedData(keystone);
+            }
+        },
     },
     lists: createSchema({
         User,
